2caa0297f52e4c23d4e01ca90dbe5d0d6aae80ac
Забиякин Макар Владимирович <M_Zabiyakin@rivs.ru>
2015-11-13 12:57:05 +0300

    started НelpDesk  app

---
+++ HelpDesk/HelpDesk/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="19f4c237-a400-49f4-96e3-f408135c9b2c" description="Компонент приложения SharePoint" featureId="19f4c237-a400-49f4-96e3-f408135c9b2c" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="HelpDesk Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="da89ef4a-5066-4a69-9d8d-87d7bfed21c5" />
+    <projectItemReference itemId="a4176a58-7c13-4b91-9998-29ff9698b3c9" />
+    <projectItemReference itemId="ea5345c6-4ac8-4a9c-9303-6da2cd968858" />
+    <projectItemReference itemId="c5d093c3-c0cf-418d-b78d-a0fd12714aed" />
+  </projectItems>
+</feature>
---
+++ HelpDesk/HelpDesk/Scripts/App.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+
+// Этот код, запускаемый после готовности модели DOM, создает объект контекста, который требуется для использования объектной модели SharePoint
+$(document).ready(function () {
+    getUserName();
+});
+
+// Эта функция подготавливает, загружает и затем выполняет запрос SharePoint для получения сведений о текущих пользователях
+function getUserName() {
+    context.load(user);
+    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+}
+
+// Эта функция выполняется, если приведенный выше вызов был успешным
+// Она заменяет содержимое элемента message именем пользователя
+function onGetUserNameSuccess() {
+    $('#message').text('Hello ' + user.get_title());
+}
+
+// Эта функция выполняется при сбое приведенного выше вызова
+function onGetUserNameFail(sender, args) {
+    alert('Failed to get user name. Error:' + args.get_message());
+}
