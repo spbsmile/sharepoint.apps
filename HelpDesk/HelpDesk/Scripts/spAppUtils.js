9fe6af5ec6d0dbdfd585dcba85ae2e3157b58b16
Makar <zabiyakin.makar@gmail.com>
2015-11-23 21:31:17 +0300

    added Andrew Connell libs

---
+++ HelpDesk/HelpDesk/Scripts/spAppUtils.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var module = {
+        getAppAbsoluteUrl: getAppAbsoluteUrl,
+        getAppRelativeUrl: getAppRelativeUrl,
+        getAppODataApiUrl: getAppODataApiUrl
+    };
+    return module;
+
+    function getAppAbsoluteUrl() {
+        return _spPageContextInfo.webAbsoluteUrl;
+    };
+
+    function getAppRelativeUrl() {
+        return _spPageContextInfo.webServerRelativeUrl;
+    };
+
+    function getAppODataApiUrl() {
+        return getAppAbsoluteUrl() + "/_api";
+    };
+});
+
+NotifyScriptLoadedAndExecuteWaitingJobs("spAppUtils.js");
