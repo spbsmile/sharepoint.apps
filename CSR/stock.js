e2a99ad28dda3569719e6e5f2b354017325a1ad4
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2015-12-25 14:39:51 +0300

    дата, рефактор.

--- CSR/stock.js
+++ CSR/stock.js
@@ -2,180 +2,176 @@
 // and attached to the XLV via JSLink property.
 
 var rowIndex = 0;
-var replaceAction = {};
+var replaceDate = {};
 var cartridgeCount = {};
-//var replaceColumnName = "_x0417__x0430__x043c__x0435__x04";
 
 var itemType;
-var listName = "market";
+var listTitle = "market";
+var countFieldName = "Count";
+var threshold = 5;
+var replaceDateFieldName = "_x0414__x0430__x0442__x0430__x00";
 //var listGuid = "4f71156b-0221-45e8-8166-7ccca783813f";
+var siteUrl = "http://devsp/sites/testdev";
 
-SP.SOD.executeFunc("clienttemplates.js", "SPClientTemplates", function() {
+SP.SOD.executeFunc("clienttemplates.js", "SPClientTemplates", function () {
 
-  function getBaseHtml(ctx) {
-    return SPClientTemplates["_defaultTemplates"].Fields.default.all.all[ctx.CurrentFieldSchema.FieldType][ctx.BaseViewID](ctx);
-  }
+    function getBaseHtml(ctx) {
+        return SPClientTemplates["_defaultTemplates"].Fields.default.all.all[ctx.CurrentFieldSchema.FieldType][ctx.BaseViewID](ctx);
+    }
 
-  function init() {
-    
-    itemType = GetItemTypeForListName(listName);
-    
-    SPClientTemplates.TemplateManager.RegisterTemplateOverrides({
+    function init() {
 
-      // OnPreRender: function(ctx) { },
+        itemType = GetItemTypeForListName(listTitle);
 
-      Templates: {
+        SPClientTemplates.TemplateManager.RegisterTemplateOverrides({
 
-      //     View: function(ctx) { return ""; },
-      //     Header: function(ctx) { return ""; },
-      //     Body: function(ctx) { return ""; },
-      //     Group: function(ctx) { return ""; },
-      //     Item: function(ctx) { return ""; },
-             Fields: {
-               "Count": {
-                   View: renderView,
-      //             EditForm: function(ctx) { return ""; },
-      //             DisplayForm: function(ctx) { return ""; },
-      //             NewForm: function(ctx) { return ""; }
-               },
-                "_x0417__x0430__x043c__x0435__x04": {
-                   View: renderReplace,
-      //             EditForm: function(ctx) { return ""; },
-      //             DisplayForm: function(ctx) { return ""; },
-      //             NewForm: function(ctx) { return ""; }
-               }
-           },
-         	// Footer: function(ctx) { return "Hello "; }
-      },
+            // OnPreRender: function(ctx) { },
 
-       OnPostRender: function(ctx) {
-    	var rows = ctx.ListData.Row;
-    	for (var i=0;i<rows.length;i++)
-    	{
-      		var isApproved = rows[i]["Count"] <= 2;
-      		console.log("hello postRender");
-      		if (isApproved)
-      		{
-        		var rowElementId = GenerateIIDForListItem(ctx, rows[i]);
-        		var tr = document.getElementById(rowElementId);
-        		tr.style.backgroundColor = "#FF0000";//"#ada"; //#FF0000
-      		}
-    	}
-  },
+            Templates: {
 
-      ListTemplateType: 100
+                //     View: function(ctx) { return ""; },
+                //     Header: function(ctx) { return ""; },
+                //     Body: function(ctx) { return ""; },
+                //     Group: function(ctx) { return ""; },
+                //     Item: function(ctx) { return ""; },
+                Fields: {
+                    "Count": {
+                        View: renderCountField,
+                        //             EditForm: function(ctx) { return ""; },
+                        //             DisplayForm: function(ctx) { return ""; },
+                        //             NewForm: function(ctx) { return ""; }
+                    },
+                    "_x0417__x0430__x043c__x0435__x04": {
+                        View: renderReplaceField,
+                        //             EditForm: function(ctx) { return ""; },
+                        //             DisplayForm: function(ctx) { return ""; },
+                        //             NewForm: function(ctx) { return ""; }
+                    }
+                },
+                // Footer: function(ctx) { return "Hello "; }
+            },
 
-    });
-  }
+            OnPostRender: function (ctx) {
+                var rows = ctx.ListData.Row;
+                for (var i = 0; i < rows.length; i++) {
+                    var little = rows[i][countFieldName] <= 2;
+                    console.log("hello postRender");
+                    if (little) {
+                        var rowElementId = GenerateIIDForListItem(ctx, rows[i]);
+                        var tr = document.getElementById(rowElementId);
+                        tr.style.backgroundColor = "#ada";//"#ada"; //#FF0000
+                    }
+                }
+            },
+            ListTemplateType: 100
+        });
+    }
 
-  RegisterModuleInit(SPClientTemplates.Utility.ReplaceUrlTokens("~siteCollection/Style Library/Market/market.js"), init);
-  init();
-  
-   function renderReplace(ctx)
-   {
-       var fieldVal = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
-       var html = "";
-       html += '<input type="button" value="Заменить" onClick="DecreaseCount(\''+ rowIndex +'\',\''+ fieldVal +'\')" />';
-       html += "</input>";
-      
-       return html;
-   }
-  
-   function renderView(ctx)
-   {
+    RegisterModuleInit(SPClientTemplates.Utility.ReplaceUrlTokens("~siteCollection/Style Library/Market/market.js"), init);
+    init();
+
+    function renderReplaceField(ctx) {
         var fieldVal = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
-      	var id = GenerateIIDForListItem(ctx, ctx.ListData.Row[rowIndex]);
-    	var itemID = id[(id.indexOf(",")+1)];
-      
+
         var html = "";
-        //html += "<input type='button' value=" \ + fieldVal + \"  />";
+        html += '<input type="button" value="Заменить" onClick="replaceAction(\'' + rowIndex + '\',\'' + fieldVal + '\')" />';
+        html += "</input>";
+        return html;
+    }
+
+    function renderCountField(ctx) {
+        var fieldVal = ctx.CurrentItem[ctx.CurrentFieldSchema.Name];
+        var id = GenerateIIDForListItem(ctx, ctx.ListData.Row[rowIndex]);
+        var itemID = id[(id.indexOf(",") + 1)];
+        var html = "";
         html += '<input type="button" value=" \'' + fieldVal + '\'  " onClick="DisplayVersionHistory(\'' + itemID + '\')" />';
         html += "</input>";
         html += "</input>";
         html += "<div id ='modalWindow';  title=' Принтер:'>";
-
-        html +="<div id='dialogText';  >";
+        html += "<div id='dialogText';  >";
         html += "";
         html += "</div>";
         html += "</div>";
         rowIndex = rowIndex + 1;
-
         return html;
     }
 });
 
-function DecreaseCount(itemID, value) {
+function replaceAction(itemID, value) {
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oList = clientContext.get_web().get_lists().getByTitle(listTitle);
 
-  	console.log(itemID + " " + value);
-    var payload = {
-      "__metadata": { "type": itemType }, 
-      "Count":  1
-    };
-  	console.log(itemType);
-    console.log("http://devsp/sites/testdev/" + "/_api/web/lists/GetByTitle('" + listName + "')/items(" + itemID + ")");
-    var p = $.ajax({
-        url: "http://devsp/sites/testdev/" + "/_api/web/lists/GetByTitle('" + listName + "')/items(" + itemID + ")",
-        method: "POST",
-        data: JSON.stringify(payload),
-        contentType: "application/json;odata=nometadata",
-        headers: {
-            "X-RequestDigest": document.getElementById("__REQUESTDIGEST").value,
-            "X-HTTP-Method": "MERGE",
-            "IF-MATCH": "*"
-        }
-    });
+    var oListItem = oList.getItemById(itemID);
+    clientContext.load(oListItem, countFieldName);
+
+    clientContext.executeQueryAsync(
+        function () {
+            oListItem.set_item(countFieldName, oListItem.get_item(countFieldName) - 1);
+            oListItem.set_item(replaceDateFieldName, moment().format('LLL'));
+            oListItem.update();
+            clientContext.executeQueryAsync(function () {
+                    console.log("success get count");
+                },
+                function () {
+                    console.log("fail get count");
+                });
+            document.location.reload();
+        },
+        function () {
+            console.log("fail get count");
+        });
 }
 
+function DisplayVersionHistory(itemID) {
 
-function DisplayVersionHistory(itemID)
-{
-    console.log(itemID + " v");
-
-	if ($("#table").length === 0)
-    {
+    if ($("#table").length === 0) {
         jQuery("#dialogText").append('<table border="1" id="table"> <caption>История изменений картриджей</caption><tr><th>Дата</th><th>Действие</th><th>Количество</th></tr></table>');
     }
 
-  
     $().SPServices({
         operation: "GetVersionCollection",
         async: false,
-        strlistID: listName,
+        strlistID: listTitle,
         strlistItemID: itemID,
-        strFieldName: "view",
+        strFieldName: replaceDateFieldName,
         completefunc: function (xData, Status) {
-            $(xData.responseText).find("Version").each(function(i) {
-                var currentValue = $(this).attr("view");
-                replaceAction[i] = currentValue;
+            $(xData.responseText).find("Version").each(function (i) {
+                replaceDate[i] = $(this).attr(replaceDateFieldName);
+                if (i >= threshold)
+                {
+                    return false;
+                }
             });
         }
     });
-    
 
     $().SPServices({
-     	operation: "GetVersionCollection",
-     	async: false,
-     	strlistID: listName,
-     	strlistItemID: itemID,
-     	strFieldName: "Count",
-     	completefunc: function (xData, Status) {
-     	$(xData.responseText).find("Version").each(function(i) {
-       
-     			cartridgeCount[i] = $(this).attr("Count");
-     		});
-     	}
-     }); 
-     
-    for(key in replaceAction)
-    {
-        $('#table').append("<tr><td>" + replaceAction[key] + "</td><td>Замена</td><td>"+ cartridgeCount[key] +"</td></tr>");
+        operation: "GetVersionCollection",
+        async: false,
+        strlistID: listTitle,
+        strlistItemID: itemID,
+        strFieldName: countFieldName,
+        completefunc: function (xData, Status) {
+            $(xData.responseText).find("Version").each(function (i) {
+                cartridgeCount[i] = $(this).attr(countFieldName);
+                if (i >= threshold)
+                {
+                    return false;
+                }
+            });
+        }
+    });
+
+    for (key in replaceDate) {
+        $('#table').append("<tr><td>" + replaceDate[key] + "</td><td>Замена</td><td>" + cartridgeCount[key] + "</td></tr>");
+        console.log(key);
     }
 
-    $(function() {
-        $( "#modalWindow" ).dialog({
+    $(function () {
+        $("#modalWindow").dialog({
             width: 600,
             //modal: true,
-            close: function(event, ui) {
+            close: function (event, ui) {
                 $("#table").remove();
             }
         });
@@ -186,4 +182,4 @@ function DisplayVersionHistory(itemID)
 function GetItemTypeForListName(name) {
     return "SP.Data." + name.charAt(0).toUpperCase() + name.split(" ").join("").slice(1) + "ListItem";
 }
-  
+
