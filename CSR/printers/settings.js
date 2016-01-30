d0252806479fc016944ea034c8d2fd2f0a543e80
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-01-30 16:44:54 +0300

    add colors input

---
+++ CSR/printers/settings.js
@@ -0,0 +1,35 @@
+(function () {
+    var module = {
+        getSiteUrl: "http://server-sp-it/sites/wiki/",
+        getListId: "e032d241-fbc2-4efb-b5bf-48b7aeaf3e67",
+        getThreshold: 20,
+        getEditingGroupId: 41,
+
+        getActionFieldName: "Action",
+
+        getCatridgeFieldName: "_x041a__x0430__x0440__x0442__x04",
+
+        getCatridgeCountFieldName: "_x041a__x043e__x043b__x0438__x04",
+
+        //на складе
+        getRemainFieldName: "_x041e__x0441__x0442__x0430__x04",
+
+        // общее число выданных
+        getNumberofissuedFieldName: "_x041e__x0431__x0449__x0435__x04",
+
+        // получатель
+        getReseivedFieldName: "_x041a__x043e__x043c__x0443__x00",
+
+        //выдано
+        getGetoutFieldName: "_x0412__x044b__x0434__x0430__x04",
+
+        // кто выдал
+        getWhogiveFieldName: "_x041a__x0442__x043e__x0020__x04",
+
+        getCommentFieldName: "_x041a__x043e__x043c__x043c__x04",
+
+        getIsColorFieldName: "IsColor"
+
+    };
+    return module;
+});
