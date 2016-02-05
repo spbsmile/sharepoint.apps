5b935dcd04bffac121fdb20ea913fae468505b63
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-02-05 17:59:22 +0300

    main page view

---
+++ main/mainNav.js
@@ -0,0 +1,37 @@
+/**
+ * Created by M_Zabiyakin on 04.02.2016.
+ */
+$(document).ready(function () {
+
+    $("#clickemployee").click(function () {
+
+        window.open("");
+    });
+
+    $("#clicknews").click(function () {
+
+        window.open("");
+    });
+
+    $("#clickdocuments").click(function () {
+
+        window.open("");
+    });
+
+    $("#clickorders").click(function () {
+
+        window.open("");
+    });
+
+    $("#clickforum").click(function () {
+
+        window.open("");
+    });
+
+    $("#clicksupport").click(function () {
+
+        window.open("");
+    });
+});
+
+
