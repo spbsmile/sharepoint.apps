f01f4f0f816471e894ae3bdd8ed8777171cdf60b
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-06-06 18:02:37 +0300

    clear folders: renames, delete

---
+++ ConsoleSqlCurrency/SqlCurrencyConsole/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SqlCurrencyConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var daysInterval = 14;
+
+            using (var db = new CurrencySP2013Entities())
+            {
+                for (var i = daysInterval; i >= 0; i--)
+                {
+                    var dateStr = DateTime.Now.AddDays(-i).ToString("dd/MM/yyyy");
+
+                    var weburl = "http://www.cbr.ru/scripts/XML_daily.asp?date_req=" + dateStr;
+                    var rdr = new XmlTextReader(weburl);
+                    var xd = XDocument.Load(rdr);
+
+                    WriteValuesToDB(db, xd, dateStr);
+                }
+                db.SaveChanges();
+            }
+        }
+
+        static void WriteValuesToDB(CurrencySP2013Entities db, XDocument xDocument, string date)
+        {
+            foreach (var valitesCode in GetAllOurIdOfCurrency(db))
+            {
+                var valute =
+                    from el in xDocument.Root.Elements("Valute")
+                    where el.Attribute("ID").Value == valitesCode.Trim()
+                    select new
+                    {
+                        value = (string) el.Elements().ElementAt(4)
+                    };
+
+                var value = "";
+                foreach (var element in valute)
+                {
+                    value = element.value;
+                }
+
+                var values_history = new values_history()
+                {
+                    primkey = date + valitesCode.Trim(),
+                    id = valitesCode,
+                    value = Convert.ToDouble(value)
+                };
+                if (!db.values_history.Any(item => item.primkey == values_history.primkey))
+                {
+                    db.values_history.Add(values_history);
+                }
+            }
+        }
+
+        static IEnumerable<string> GetAllOurIdOfCurrency(CurrencySP2013Entities db)
+        {
+            return db.description.Select(description => description.id).ToList();
+        }
+    }
+}
--- SharePointCurrencyEmpty/SharePointCurrencyEmpty/Features/Feature1/Feature1.feature
+++
@@ -1,6 +0,0 @@
-﻿<?xml version="1.0" encoding="utf-8"?>
-<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="b92f700d-3d53-4dc7-9207-f5241a423658" featureId="b92f700d-3d53-4dc7-9207-f5241a423658" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="SharePointCurrencyEmpty Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
-  <projectItems>
-    <projectItemReference itemId="93ba301b-2b22-4ce0-ac45-02ae4b7dbd8a" />
-  </projectItems>
-</feature>
