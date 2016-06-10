1a6f66cfa3eea20ba2ef255d7d3e3cd7c14a7a64
Makar Zabiyakin <Makar Zabiyakin>
2016-06-10 09:07:21 +0300

    snap

---
+++ mySqlTest/mySqlTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace mySqlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DataModelEntities db = new DataModelEntities();
+            //Console.WriteLine(db.test_table.First().name);
+            Console.ReadLine();
+        }
+    }
+}
