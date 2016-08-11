9fda28e4fb908ad557ca040291f1e1694ddaded5
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-08-11 10:21:31 +0300

    coreCalendar improve

--- coreCalendar.js
+++ coreCalendar.js
@@ -4,6 +4,7 @@
 var currentIterateMonth = null;
 var dataEmployee = null;
 var shiftStartDate = null;
+var thresholdCountDaysHirePersons = 7;
 
 $(document).ready(function () {
     moment.tz.add("Europe/Moscow|MSK MSD MSK|-30 -40 -40|01020|1BWn0 1qM0 WM0 8Hz0|16e6");
@@ -31,6 +32,8 @@ $(document).ready(function () {
     $("#btnPrevMonth").click(function () {
         currentIterateMonth--;
         var shiftStartDate = parseInt(moment([moment().format('YYYY'), currentIterateMonth]).weekday());
+        setCellsOfBirthdaysEmployes(currentIterateMonth, shiftStartDate);
+
         setCellCalendar(shiftStartDate, currentIterateMonth);
         setMonthViewForIterate(currentIterateMonth);
         return false;
@@ -39,6 +42,8 @@ $(document).ready(function () {
     $("#btnNextMonth").click(function () {
         currentIterateMonth++;
         var shiftStartDate = parseInt(moment([moment().format('YYYY'), currentIterateMonth]).weekday());
+        setCellsOfBirthdaysEmployes(currentIterateMonth, shiftStartDate);
+
         setCellCalendar(shiftStartDate, currentIterateMonth);
         setMonthViewForIterate(currentIterateMonth);
         return false;
@@ -46,23 +51,17 @@ $(document).ready(function () {
 });
 
 function setMonthViewForIterate(indexMonth) {
+    //if sep top css
     if (indexMonth === parseInt(moment().format('M') - 1)) {
         setCurrentMonthView();
+        //sept month widly
     } else {
-        $('#DayNow').html(moment(new Date(moment().format('YYYY'), indexMonth, 04)).format("MMMM YYYY"));
-        $(".ring-left, .ring-right").css({top: '66px'});
-        $(".chevron").css({bottom: '71.5%'});
+        $('#DayNow').html(moment(new Date(moment().format('YYYY'), indexMonth, 04)).format("MMMM") + " <br>" + moment(new Date(moment().format('YYYY'), indexMonth, 04)).format('YYYY'));
     }
 }
 
 function setCurrentMonthView() {
-    var currentDate = moment().format("D MMMM").toString();
-    var array = currentDate.split(" ");
-    $('#DayNow').html(moment().format("D") + "&nbsp" + array[1] + "&nbsp" + "2016" + "<br/>" +
-        capitalizeFirstLetter(moment().format('dddd')));
-
-    $(".ring-left, .ring-right").css({top: '95px'});
-    $(".chevron").css({bottom: '65.5%'});
+    $('#DayNow').html(moment().format("MMMM") + " <br>" + moment().format('YYYY'));
 }
 
 function successHandler(data) {
@@ -71,31 +70,30 @@ function successHandler(data) {
     var results = data.d.query.PrimaryQueryResult.RelevantResults.Table.Rows.results;
 
     dataEmployee = results;
+    var thresholdDateHirePersons = moment().day(-thresholdCountDaysHirePersons).format("YYYY-MM-DD");
 
-    var thresholdDateHirePersons = moment().day(-14).format("YYYY-MM-DD");
+    //icon current day
+    $("#cell_" + (parseInt(moment().format('D')) + shiftStartDate - 1)).addClass('day_current');
     // iterate of all users
     for (var i = 0; i < results.length; i++) {
-        // define new hire persons
+        var name = results[i].Cells.results[2].Value;
+        var job = results[i].Cells.results[3].Value;
+        var department = results[i].Cells.results[4].Value;
+        var birthday = results[i].Cells.results[5].Value;
+        var pictureUrl = results[i].Cells.results[6].Value;
         var hireDate = results[i].Cells.results[7].Value;
-        if (moment(hireDate).isAfter(thresholdDateHirePersons)) {
+
+        // define new hire persons
+        if (moment(hireDate).isAfter(thresholdDateHirePersons) && department != "Комнаты переговоров") {
             if (!newEmploeeInit) {
                 $("#newEmployee_block").append('<h1> Новые Сотрудники </h1>');
                 newEmploeeInit = true;
             }
-
-            var name = results[i].Cells.results[2].Value;
-            var department = results[i].Cells.results[4].Value;
-            var job = results[i].Cells.results[3].Value;
-            var pictureUrl = results[i].Cells.results[6].Value;
             displayWidgetEmployee("#newEmployee_block", name, job, department, pictureUrl);
         }
 
-        var birthday = results[i].Cells.results[5].Value;
-        //icon current day
-        $("#cell_" + (parseInt(moment().format('D')) + shiftStartDate - 1)).addClass('day_current');
         //filter of current month birthday field
         if (moment(birthday, 'DD.MM.YYYY').isValid() && moment(birthday, 'DD.MM.YYYY').month() === moment().month()) {
-            var name = results[i].Cells.results[2].Value;
             var numberDayBirthday = moment(birthday, 'DD.MM.YYYY').format('D');
             var indexCell = (parseInt(numberDayBirthday) + shiftStartDate - 1);
             // birthday cell of calendar
@@ -107,9 +105,6 @@ function successHandler(data) {
                     congratsBirthdayInit = true;
                 }
                 $(id).addClass('birthday_current');
-                var department = results[i].Cells.results[4].Value;
-                var job = results[i].Cells.results[3].Value;
-                var pictureUrl = results[i].Cells.results[6].Value;
                 displayWidgetEmployee("#birthday_block", name, job, department, pictureUrl);
             }
             $(id).addClass('birthday');
@@ -123,12 +118,42 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function defineHirePersons() {
+function setCellsOfBirthdaysEmployes(iterateMonth, shiftStartDate) {
 
-}
+    // clear all cells
+    for (var i = 0; i <= 41; i++) {
+        var id = "#cell_" + i;
+        $(id).text("");
+        $(id).removeClass();
+        $(id).removeAttr("title");
+    }
 
-function setCellsOfBirthdaysEmployes() {
+    //icon current day
+    if (iterateMonth === parseInt(moment().format('M') - 1)) {
+        $("#cell_" + (parseInt(moment().format('D')) + shiftStartDate - 1)).addClass('day_current');
+    }
 
+    var results = dataEmployee;
+    // iterate of all users
+    for (var i = 0; i < results.length; i++) {
+
+        var birthday = results[i].Cells.results[5].Value;
+        //filter of current month birthday field
+        if (moment(birthday, 'DD.MM.YYYY').isValid() && moment(birthday, 'DD.MM.YYYY').month() === iterateMonth) {
+            var name = results[i].Cells.results[2].Value;
+            var numberDayBirthday = moment(birthday, 'DD.MM.YYYY').format('D');
+            var indexCell = (parseInt(numberDayBirthday) + shiftStartDate - 1);
+            // birthday cell of calendar
+            var id = "#cell_" + indexCell;
+            // congrat today birthdays
+            if (moment().format('D') === numberDayBirthday) {
+                $(id).addClass('birthday_current');
+            }
+            $(id).addClass('birthday');
+            var prevName = $(id).prop("title") && $(id).prop("title").split(':')[1] ? ", " + $(id).prop("title").split(':')[1] : " ";
+            $(id).attr('title', 'День Рождения: ' + name + prevName);
+        }
+    }
 }
 
 function setCellCalendar(shift, currentMonth) {
@@ -167,12 +192,12 @@ function displayWidgetEmployee(containerId, name, jobTitle, department, photo) {
         photoImprove = "/_layouts/15/CustomjsLibs/1.devsp/noPhoto.jpg";
     }
     var fio = name.split(" ");
-    $(containerId).append('<div class="employeeRow"> <div class="employeeCell"> <div class="wrap_employeeCell"> <div class="wrap_employeeCell">' +
-        ' <p> <img src=' + photoImprove + '>' + '</p>' +
+    $(containerId).append('<div class="employeeRow"> <div class="employeeCell"> <div class="wrap_employeeCell "> ' +
+        ' <a class="thumbnailMainPage"> <img src=' + photoImprove + '>' + '</a>' +
         '<h2 class="title_name">' +
         '<p>' + fio[0] + '<br>' + fio[1] + '<br>' + fio[2] + '</p> </h2> ' +
-        '<div class="empl_jobs_title"> <p> ' + department + ' </p>' +
-        '<p> ' + jobTitle + ' </p> </div></div></div></div>'
+        '<div class="empl_jobs_title"> <span class="department_inwidget"> ' + department + '</br> </span>' +
+        '<span class="jobTitle_inwidget"> ' + jobTitle + ' </span> </div></div></div>'
     );
 }
 
