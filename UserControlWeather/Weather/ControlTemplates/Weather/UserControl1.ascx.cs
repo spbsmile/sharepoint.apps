37501ec067a29f3ae4c10dc1a47dd26068e49245
Makar Zabiyakin <zabiyakin.makar@gmail.com>
2016-09-20 17:00:15 +0300

    rename usercontrolweather

--- UserControlWeather/Weather/ControlTemplates/Weather/UserControl1.ascx.cs
+++ UserControlWeather/Weather/ControlTemplates/Weather/UserControl1.ascx.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Net;
-using System.Text;
 using System.Web.UI;
-using Newtonsoft.Json.Linq;
 
 namespace Weather.ControlTemplates.Weather
 {
